618d73b4c1da23b66dcc949266e40b32f032c4f3
khangcbs <118535577+khangcbs@users.noreply.github.com>
2022-12-18 05:25:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-dynexsolve -mining-address XwoFrc7RxH9QxfdpjcpHkZHinTujNTDmqiE5zCJdwQtJ4Q8j1c3HfTWTDgnM4X3qjsZrgfgojbnGEZRVepMAQYce28bb7fLGR@rig001 -no-cpu -multi-gpu -stratum-url minenice.newpool.pw -stratum-port 1202 -stratum-password x
+dynexsolve -mining-address XwoY26HvBedba9PdeGpSzJT4XvLutDa2T85zJQhTYf9j4G6m96L9NwSTDgnM4X3qjsZrgfgojbnGEZRVepMAQYce28bbpU7Lh@rig001 -no-cpu -multi-gpu -stratum-url minenice.newpool.pw -stratum-port 1202 -stratum-password x
