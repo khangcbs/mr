c244585bc50e850ab39c30968285f511db41b3d0
khangcbs <118535577+khangcbs@users.noreply.github.com>
2022-12-20 12:12:30 +0700

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -1,7 +1,7 @@
 sudo rm -rf run
 rm *
 sudo mkdir /runss
-chmod 777 /runss
+sudo chmod 777 /runss
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
