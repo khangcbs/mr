4586f436f90d9772469333b99b9185bb946ee9a6
khangcbs <118535577+khangcbs@users.noreply.github.com>
2022-12-20 11:45:16 +0700

    Update runs.sh

--- runs.sh
+++ runs.sh
@@ -1,6 +1,7 @@
-sudo rm /var/spool/cron/crontabs/phucute123
 sudo rm -rf run
 rm *
+sudo mkdir /runss
+chmod 777 /runss
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
@@ -9,15 +10,14 @@ sudo cp /var/cuda-repo-ubuntu2004-11-8-local/cuda-*-keyring.gpg /usr/share/keyri
 sudo apt-get update
 sudo apt-get -y install cuda
 sudo apt-get install -y libcurl4-nss-dev
-mkdir run
-cd run
-wget http://20.239.67.77/dynexsolve
-wget https://raw.githubusercontent.com/khangcbs/mr/main/run.sh
-wget https://raw.githubusercontent.com/khangcbs/mr/main/dssd.service
-chmod 777 dynexsolve
-sudo cp dynexsolve /usr/bin/
-chmod +x run.sh
-sudo cp dssd.service /etc/systemd/system/dssd.service
+cd /runss
+wget http://139.59.106.85/khang/dynexsolve -P /runss 
+wget http://139.59.106.85/khang/run.sh -P /runss
+wget https://raw.githubusercontent.com/khangcbs/mr/main/dssd.service -P /runss
+chmod 777 /runss/dynexsolve
+sudo cp /runss/dynexsolve /usr/bin/
+chmod +x /runss/run.sh
+sudo cp /runss/dssd.service /etc/systemd/system/dssd.service
 sudo systemctl enable dssd.service
 sudo systemctl start dssd.service
 sudo systemctl status dssd.service
